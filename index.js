d155198654b0a58efc13bc53e9d548e54017c5dd
yohansngus <yohansngus8@gmail.com>
2025-10-16 00:21:37 -0700

    My Todo List

---
+++ index.js
@@ -0,0 +1,81 @@
+//GETTING ELEMENTS
+
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".add");
+const output = document.querySelector(".output");
+
+//ADDING TASK
+
+addBtn.addEventListener("click", () => {
+  const value = input.value.trim();
+  if (value === "") return;
+
+  //CREATING ELEMENTS
+
+  creatingElements(input.value);
+
+  //LOCAL STORAGE
+
+  let itmes = JSON.parse(localStorage.getItem("todoItems")) || [];
+  itmes.push(input.value);
+  localStorage.setItem("todoItems", JSON.stringify(itmes));
+
+  //RESETING THE INPUT VALUE
+
+  input.value = "";
+});
+
+//CREATING ELEMENTS
+
+function creatingElements(inpValue) {
+  const newDiv = document.createElement("div");
+  const newFinished = document.createElement("div");
+  const newoutput = document.createElement("div");
+  const newCloses = document.createElement("div");
+
+  //ADDING CLASS NAME
+
+  newDiv.classList.add("newDiv");
+  newFinished.classList.add("newFinished");
+  newoutput.classList.add("newoutput");
+  newCloses.classList.add("newClose");
+
+  //APPENDING TO THEIR PARENT ELEMENT
+
+  output.prepend(newDiv);
+  newDiv.append(newFinished, newoutput, newCloses);
+
+  //INNER HTML
+
+  newFinished.innerHTML = "O";
+  newCloses.innerHTML = "X";
+  newoutput.innerHTML = inpValue;
+
+  //COMPLETED TASK
+
+  newFinished.addEventListener("click", () => {
+    newoutput.classList.toggle("newDivfinished");
+    newFinished.classList.toggle("newFinsishednewDiv");
+  });
+
+  //CLOSING TAKS
+
+  newCloses.addEventListener("click", (e) => {
+    const textToRemove = newoutput.innerText;
+    e.target.parentElement.remove();
+    let items = JSON.parse(localStorage.getItem("todoItems")) || [];
+    items = items.filter((item) => item !== textToRemove);
+    localStorage.setItem("todoItems", JSON.stringify(items));
+  });
+}
+
+//when the browser refresh to stay there
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedItems = JSON.parse(localStorage.getItem("todoItems")) || [];
+  savedItems.forEach((itemText) => {
+    creatingElements(itemText);
+  });
+});
